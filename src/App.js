efbd874f6df5fd8114331fc498606be1f523c4ae
berkezircan <berkezircan@gmail.com>
2019-07-12 01:39:15 +0300

    Add Log Modal added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
+// Materialize
 import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js'
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
   useEffect(() => {
     // Init Materialize Js
     M.AutoInit();
-  })
+  });
 
   return (
     <Fragment>
-      <SearchBar /> 
-      <div className="container">
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
-      </div>
+      </div>{' '}
     </Fragment>
-  ) 
-
-}
+  );
+};
 
 export default App;
