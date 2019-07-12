0c8b70c7c627230f1fcced58c4f267d6f8e8fe78
berkezircan <berkezircan@gmail.com>
2019-07-12 19:52:15 +0300

    delete log functionality added

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { deleteLog } from '../../actions/logActions';
 import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: 'Log Deleted' });
+  };
+
   return (
     <li className='collection-item'>
       <div>
@@ -12,24 +20,27 @@ const LogItem = ({ log }) => {
                      ${log.attention ? 'red-text' : 'blue-text'}
                      `}
         >
-          {log.message}{' '}
-        </a>{' '}
+          {log.message}
+        </a>
         <br />
         <span className='grey-text'>
           <span className='black-text'> ID# {log.id} </span> last updated by{' '}
           <span className='black-text'> {log.tech} </span> on{' '}
           <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
-        </span>{' '}
-        <a href='#!' className='secondary-content'>
-          <i className='material-icons grey-text'> delete </i>{' '}
-        </a>{' '}
-      </div>{' '}
+        </span>
+        <a href='#!' className='secondary-content' onClick={onDelete}>
+          <i className='material-icons grey-text'> delete </i>
+        </a>
+      </div>
     </li>
   );
 };
 
-export default LogItem;
-
 LogItem.propTypes = {
   log: PropTypes.object.isRequired
 };
+
+export default connect(
+  null,
+  { deleteLog }
+)(LogItem);
