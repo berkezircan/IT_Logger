180a055d1de8b2e142371bab71952702c118e243
berkezircan <berkezircan@gmail.com>
2019-07-12 23:27:40 +0300

    Search functionality added

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { searchLogs } from '../../actions/logActions';
 
-const SearchBar = () => {
-    return (
-        <nav style={{ marginBottom : '30px'}} className="blue">
-            <div className="nav-wrapper">
-                <form>
-                    <div className="input-field">
-                        <input type="search" id="search"/>
-                        <label className="label-icon" htmlFor="search">
-                            <i className="material-icons">search</i>
-                        </label>
-                    </div>
-                </form>
-            </div>
-        </nav>
-    )
-}
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
 
-export default SearchBar
+  const onChange = () => {
+    searchLogs(text.current.value);
+  };
+
+  return (
+    <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input
+              type='search'
+              id='search'
+              placeholder='Search logs'
+              ref={text}
+              onChange={onChange}
+            />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { searchLogs }
+)(SearchBar);
