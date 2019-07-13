ca61205e7e0c2c2bcd8c69f0963da89e7b086e2a
berkezircan <berkezircan@gmail.com>
2019-07-13 03:59:11 +0300

    tech options select component added

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { updateLog } from '../../actions/logActions';
 
 const EditLogModal = ({ current, updateLog }) => {
@@ -62,12 +63,7 @@ const EditLogModal = ({ current, updateLog }) => {
               className='browser-default'
               onChange={e => setTech(e.target.value)}
             >
-              <option value='' disabled>
-                Select Technician
-              </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
